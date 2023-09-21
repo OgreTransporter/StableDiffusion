ad9a6dbcd61f753fc122bb190f5d646526636b18
sa_ddam213 <sa_ddam213@live.com>
2023-09-21 17:38:49 +1200

    Upgrade to .NET 7, update example app

--- StableDiffusion/Program.cs
+++ StableDiffusion/Program.cs
@@ -1,52 +1,65 @@
-﻿using StableDiffusion.ML.OnnxRuntime;
+﻿using StableDiffusion.ML.OnnxRuntime.Inference;
+using System.Diagnostics;
 
 namespace StableDiffusion
 {
     public class Program
     {
-        static void Main(string[] args)
+        static void Main(string[] _)
         {
             //test how long this takes to execute
-            var watch = System.Diagnostics.Stopwatch.StartNew();
+            var timestamp = Stopwatch.GetTimestamp();
 
             //Default args
             var prompt = "a fireplace in an old cabin in the woods";
-            Console.WriteLine(prompt);
+            var negativePrompt = "stone, chair";
+
+            Console.WriteLine($"Prompt: {prompt}");
+            Console.WriteLine($"NegativePrompt: {negativePrompt}");
 
             var config = new StableDiffusionConfig
             {
-                // Number of denoising steps
+                // The number of steps to run inference for.
+                // The more steps the longer it will take to run the inference loop but the image quality should improve.
                 NumInferenceSteps = 15,
-                // Scale for classifier-free guidance
+
+                // The scale for the classifier-free guidance.
+                // The higher the number the more it will try to look like the prompt but the image quality may suffer.
                 GuidanceScale = 7.5,
+
                 // Set your preferred Execution Provider. Currently (GPU, DirectML, CPU) are supported in this project.
                 // ONNX Runtime supports many more than this. Learn more here: https://onnxruntime.ai/docs/execution-providers/
                 // The config is defaulted to CUDA. You can override it here if needed.
                 // To use DirectML EP intall the Microsoft.ML.OnnxRuntime.DirectML and uninstall Microsoft.ML.OnnxRuntime.GPU
-                ExecutionProviderTarget = StableDiffusionConfig.ExecutionProvider.Cuda,
+                ExecutionProviderTarget = StableDiffusionConfig.ExecutionProvider.DirectML,
+
                 // Set GPU Device ID.
                 DeviceId = 0,
+
                 // Update paths to your models
-                TextEncoderOnnxPath = @"C:\code\StableDiffusion\StableDiffusion\models\text_encoder\model.onnx",
-                UnetOnnxPath = @"C:\code\StableDiffusion\StableDiffusion\models\unet\model.onnx",
-                VaeDecoderOnnxPath = @"C:\code\StableDiffusion\StableDiffusion\models\vae_decoder\model.onnx",
-                SafetyModelPath = @"C:\code\StableDiffusion\StableDiffusion\models\safety_checker\model.onnx",
+                TextEncoderOnnxPath = @"D:\Repositories\stable-diffusion-v1-5\text_encoder\model.onnx",
+                UnetOnnxPath = @"D:\Repositories\stable-diffusion-v1-5\unet\model.onnx",
+                VaeDecoderOnnxPath = @"D:\Repositories\stable-diffusion-v1-5\vae_decoder\model.onnx",
+                SafetyModelPath = @"D:\Repositories\stable-diffusion-v1-5\safety_checker\model.onnx",
+
+                // Is Anti-NSFW Enabled
+                IsSafetyEnabled = false
             };
 
             // Inference Stable Diff
-            var image = UNet.Inference(prompt, config);
-
-            // If image failed or was unsafe it will return null.
-            if (image == null)
+            using (var uNet = new UNet(config))
             {
-                Console.WriteLine("Unable to create image, please try again.");
+                var image = uNet.Inference(prompt, negativePrompt);
+
+                // If image failed or was unsafe it will return null.
+                if (image == null)
+                {
+                    Console.WriteLine("Unable to create image, please try again.");
+                }
             }
+
             // Stop the timer
-            watch.Stop();
-            var elapsedMs = watch.ElapsedMilliseconds;
-            Console.WriteLine("Time taken: " + elapsedMs + "ms");
-
+            Console.WriteLine($"Time taken: {Stopwatch.GetElapsedTime(timestamp).TotalMilliseconds}ms");
         }
-
     }
 }
